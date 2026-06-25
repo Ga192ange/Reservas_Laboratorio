4b953018d4ef1041760b59e8884fbf215aac9ab3
Angelica Garcia <angelicagarcia1908@gmail.com>
2026-06-25 14:56:52 -0500

    Configurar Firebase con variables de entorno

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,16 @@
+import os
+import json
 import firebase_admin
-from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase-key.json")
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+firebase_credentials = os.environ.get("FIREBASE_CREDENTIALS")
+
+cred_dict = json.loads(firebase_credentials)
+
+cred = credentials.Certificate(cred_dict)
+
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
